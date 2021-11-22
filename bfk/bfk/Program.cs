cabb1fddfd2f7b7cbedc030c40adefd9262e3ee1
Kaya <87698276+KayaMagda@users.noreply.github.com>
2021-11-22 09:26:00 +0100

    Add files via upload

---
+++ bfk/bfk/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace bfk
+{
+    class Program
+    {
+        private static string command;
+        static void Main(string[] args)
+        {
+           if(args.Length < 1)
+            {
+                Console.WriteLine("Bitte gib deinen Befehl ein:");
+                string uncleanedinput = Console.ReadLine();
+                string deletedwhitespace = uncleanedinput.Replace(" ", "");
+                command = deletedwhitespace.ToLower();
+                Console.WriteLine(command);
+            }
+            else
+            {
+                string uncleaned = args[0];
+                string deletedwhitespace = uncleaned.Replace(" ", "");
+                command = deletedwhitespace.ToLower();
+                Console.WriteLine(command);
+
+            }
+            Console.WriteLine("Drücke irgendeine Taste zum Beenden");
+            Console.ReadKey();
+        }
+    }
+}
