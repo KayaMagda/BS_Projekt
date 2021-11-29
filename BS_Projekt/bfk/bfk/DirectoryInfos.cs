cc339e925719a93af7c41de711fee931411224ba
Kaya <87698276+KayaMagda@users.noreply.github.com>
2021-11-29 09:32:01 +0100

    Add files via upload

---
+++ BS_Projekt/bfk/bfk/DirectoryInfos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace bfk
+{
+    class DirectoryInfos
+    {
+        public static List<string> directoryNames(string[] directories)
+        {
+            List<string> names = new List<string>();
+            foreach(string directorypath in directories)
+            {
+                DirectoryInfo di = new DirectoryInfo(directorypath);
+                names.Add(di.Name);
+                
+
+            }
+            return names;
+        }
+    }
+}
